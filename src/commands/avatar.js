10b06e00ea018a3788b1d290aac44d729a75f1e3
PeteWein <51336627+PeteWein@users.noreply.github.com>
2020-08-05 19:57:40 -0400

    Minor bug fixes (#3)
    * left a command, voiding aliases
    
    * fix alias property name
    
    * fix file spacing

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'avatar',
 	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
-	alias: ['icon'],
+	aliases: ['icon'],
 	execute(message) {
 		if (!message.mentions.users.size) {
 			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
