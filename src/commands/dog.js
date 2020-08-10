c1764c733eb5de01be6358529a7942f5643d97cc
Peter Wein <peter.j.wein@gmail.com>
2020-08-09 22:26:28 -0400

    added new cmds

---
+++ src/commands/dog.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+// This one was actually a little annoying
+module.exports = {
+  name: 'dog',
+  description: 'Get a random dog!',
+  execute(message) {      
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      message.channel.send(response.data.message);
+    })
+    .catch(error => {
+      console.log(error);
+      return message.channel.send(`I'm unable to grab a dog, sorry master ${message.author}`);
+    }); 
+  }
+};
