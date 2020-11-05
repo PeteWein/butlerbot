d489cce2497421b362571da8f9d5f6d79529aca6
Peter Wein <peter.j.wein@gmail.com>
2020-11-05 11:27:12 -0500

    added log level option

--- src/butlerbot.js
+++ src/butlerbot.js
@@ -99,6 +99,7 @@ client.once('ready', () => {
 
 // create our logging objects and levels
 const logger = winston.createLogger({
+	level: process.env.LOG_LEVEL,
 	transports: [
 		new winston.transports.Console()
 	],
@@ -110,6 +111,7 @@ client.on('debug', m => logger.log('debug', m));
 client.on('warn', m => logger.log('warn', m));
 client.on('error', m => logger.log('error', m));
 process.on('uncaughtException', error => logger.log('error', error));
+
 
 // login to Discord with your app's token and begin running
 client.login(process.env.BOT_TOKEN);
