7eb509992f3dddc28c9e91b840627d4d997c86ae
Peter Wein <51336627+PeteWein@users.noreply.github.com>
2021-01-03 17:49:38 -0500

    Unittest addition (#24)
    * added log level to env example
    
    * added log level option
    
    * updating unit tests
    
    * fixed formatting/added source material
    
    * updated readme, fixed some typos
    
    * duplicate command
    
    * updated help cmd format, added desc
    
    * added local dev mode for nodemon
    
    * removed unnecessary files
    
    * moved log level to config.json from .env
    
    * added code coverage
    
    * added badge
    
    * added fail api call unittest
    
    * added coverage to gitignore
    
    * updated test to expect undefined result
    
    * double await fix
    
    * added apology unittest
    
    * butlerbot message unittest
    
    * additional unittests
    
    * fixed unittest
    
    * remove node 10.x testing

--- src/butlerbot.js
+++ src/butlerbot.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const winston = require('winston');
-const { prefix } = require('../config.json');
+const { prefix, logLevel } = require('../config.json');
 require('dotenv').config();
 
 // create a new Discord objects
@@ -99,6 +99,7 @@ client.once('ready', () => {
 
 // create our logging objects and levels
 const logger = winston.createLogger({
+	level: `${logLevel}`,
 	transports: [
 		new winston.transports.Console()
 	],
@@ -110,6 +111,7 @@ client.on('debug', m => logger.log('debug', m));
 client.on('warn', m => logger.log('warn', m));
 client.on('error', m => logger.log('error', m));
 process.on('uncaughtException', error => logger.log('error', error));
+
 
 // login to Discord with your app's token and begin running
 client.login(process.env.BOT_TOKEN);
