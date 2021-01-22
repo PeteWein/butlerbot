61ca54d40c1cc532614c2759b263b9ec42cc0c62
Peter Wein <peter.j.wein@gmail.com>
2021-01-22 09:41:54 -0500

    correct roll parsing

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -12,7 +12,7 @@ module.exports = {
 	name: 'roll',
     description: 'Roll a die or multiple dice!',
     aliases: ['dice'],
-    usage: '#dType, ex: 1d6, 1d6+1d12, 1d3, 2d8',
+    usage: '#dType, ex: 1d6, 2d8, 1d6 + 1d12, 1d4+1d6, 1d4 1d12',
     args: true,
     cooldown: 0.1,
     execute(message,args) {   
@@ -21,14 +21,26 @@ module.exports = {
          * dieTotal: total die roll, starting at 0 and incremented up based on die roll
          * diceArray: where we will prep and store the dice roll
          * diceArray: results of each dice result for additional info in the roll
+         * userRoll: the user input for the roll command
          */
         let dieTotal = 0;
         let dieArray = [];
         let diceArray = [];
+        let userRoll =  args !== undefined ? args : []; 
+        /*
+         * check if its one argument (no spaces) and has a plus (multiple dice)
+         * if 0 args, program will fail since args are requires
+         * if 1 arg, check if its a 1 dice roll or multipled dice without whitespace
+         * if it is missing spaces, tell the user and exit the function early
+         */
+        if (userRoll.length === 1 && userRoll[0].indexOf('+') > -1) {
+            userRoll = userRoll[0].split('+');
+        }        
+        
         // grab the raw input and remove bad array values/split them/etc. 
         let i = 0;
-        for (i = 0; i < args.length; i++) {
-            let roll = args[i].toLowerCase().split('d');
+        for (i = 0; i < userRoll.length; i++) {
+            let roll = userRoll[i].toLowerCase().split('d');
             if (!roll.includes('+')) {
                 dieArray.push(roll);
             }
