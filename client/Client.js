47e3af9ac6914f6f989fa4cdcffc0aa4889f61d0
Peter Wein <peter.j.wein@gmail.com>
2021-05-17 12:51:38 -0400

    add class ext for discord.js client

---
+++ client/Client.js
@@ -0,0 +1,13 @@
+const { Client, Collection } = require('discord.js');
+
+module.exports = class extends Client {
+	constructor(config) {
+		super({
+			disableEveryone: true,
+			disabledEvents: ['TYPING_START'],
+		});
+		this.commands = new Collection();
+		this.queue = new Map();
+		this.config = config;
+	}
+};
