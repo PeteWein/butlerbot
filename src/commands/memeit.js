15d52d62ec27a449ecddebdc3ae63024feda9e18
Peter Wein <peter.j.wein@gmail.com>
2021-05-19 00:48:20 -0400

    initial commit

--- src/commands/memeit.js
+++ src/commands/memeit.js
@@ -4,6 +4,8 @@
  * @return {Object} - image (as a message to discord text channel)
  */
 const axios = require('axios');
+const queryString = require('query-string');
+
 module.exports = {
   name: 'memeit',
   description: 'Overlay previous message onto a meme',
@@ -26,34 +28,37 @@ module.exports = {
       });
       const rand = Math.floor(Math.random() * memeList.length);
       response = memeList[rand];
-      console.log(response);
       return response.id;
     })        
     .catch(error => {
       console.log(error);
       return message.channel.send(`I'm unable to meme-ify the message, sorry master ${message.author}`);
     });
+    memePromise.then(memeId => {
+      message.channel.messages.fetch({ limit: 2 }).then(messages => {
+        let body = queryString.stringify({
+          'username': process.env.IMGFLIP_USERNAME,
+          'password': process.env.IMGFLIP_PASSWORD,
+          'template_id': memeId,
+          'text0': messages.last().content,
+          'text1': ''
+        });
+        return body;
+      })
+      .then((captionBody) => {
+        let captionApi = 'https://api.imgflip.com/caption_image';     
+        // axios promise as var
+        axios.post(captionApi + '?' + captionBody)
+        .then(response => {
+          console.log(response.data);
 
-    // same deal but for the captioned meme
-    let captionApi = 'https://api.imgflip.com/caption_image';     
-    // axios promise as var
-    let captionPromise = axios.post(captionApi)
-    .then(response => {
-      var memeList = response.data.data.memes.filter(function (meme) {
-        return meme.box_count <= 2;
-      });
-      const rand = Math.floor(Math.random() * memeList.length);
-      response = memeList[rand];
-      return response.id;
-    })        
-    .catch(error => {
-      console.log(error);
-      return message.channel.send(`I'm unable to caption the message, sorry master ${message.author}`);
-    });
-    
-    // main call
-    memePromise.then(meme => {
-      console.log(meme);
+          message.channel.send(response.data.data.url);
+        })        
+        .catch(error => {
+          console.log(error);
+          return message.channel.send(`I'm unable to caption the message, sorry master ${message.author}`);
+        });
+      });  
     });
   }
 };
